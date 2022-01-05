13ec9d6c4d05a50ff8e2f579e2f33866fb7fe9e1
rabie.badrouch@digit-u.com <rabie.badrouch@digit-u.com>
2022-01-05 17:13:18 +0100

    // heloo cimmit 3

--- src/main/java/com/jenkiss/job/jenkinsjob/JenkinsjobApplication.java
+++ src/main/java/com/jenkiss/job/jenkinsjob/JenkinsjobApplication.java
@@ -8,7 +8,7 @@ public class JenkinsjobApplication {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello ! ");
+        System.out.println("Hello ! wzaaa");
         SpringApplication.run(JenkinsjobApplication.class, args);
     }
 
