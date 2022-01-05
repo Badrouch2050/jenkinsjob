7cacaf466a4bec84af5338359ff919679ee4e5b7
rabie.badrouch@digit-u.com <rabie.badrouch@digit-u.com>
2022-01-05 14:38:04 +0100

    Initial commit

---
+++ src/main/java/com/jenkiss/job/jenkinsjob/JenkinsjobApplication.java
@@ -0,0 +1,14 @@
+package com.jenkiss.job.jenkinsjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsjobApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkiss/job/jenkinsjob/JenkinsjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkiss.job.jenkinsjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
